4e976eb79e155ae4012e605062b48353e82c7763
paulo ricardo <pauloricardojm@hotmail.com.br>
2017-10-29 20:45:09 -0200

    Added remove and search functions

--- inc/skip_list.h
+++ inc/skip_list.h
@@ -2,8 +2,12 @@
     #define SKIP_LIST_H
 
         typedef struct t_skip_list SKIP_LIST;
+        typedef struct t_node NODE;
 
-        SKIP_LIST *create();
-        void insert(SKIP_LIST **list, int i);
-        void delete(SKIP_LIST **list);
+        SKIP_LIST *skip_list_create();
+        void skip_list_insert(SKIP_LIST **list, int i);
+        NODE *skip_list_search(SKIP_LIST *list, int n);
+        void skip_list_print_node(NODE *nd);
+        void skip_list_remove(SKIP_LIST *list, int n);
+        void skip_list_delete(SKIP_LIST **list);
 #endif
